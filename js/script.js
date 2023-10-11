af119ff8d19fce392c022070ecbb0b266a72edda
MaxPredathor <max.gilli98@gmail.com>
2023-10-11 14:39:49 +0200

    css styling

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-let i;
+let x;
 
-for(i = 1; 1 < 101; i++) {
-    if (i % 3 === 0 && i % 5 === 0){
+
+for(x = 1; x < 101; x++) {
+    if (x % 3 === 0 && x % 5 === 0){
         console.log('FizzBuzz')
-    } else if(i % 3 === 0){
+    } else if(x % 3 === 0){
         console.log('Fizz')
-    } else if(i % 5 === 0){
+    } else if(x % 5 === 0){
         console.log('Buzz')
     } else {
-        console.log(i)
+        console.log(x)
     }
 
 }

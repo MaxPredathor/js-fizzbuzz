0afa44da8d1f01caed5f9c0928faed254adc80eb
MaxPredathor <max.gilli98@gmail.com>
2023-10-11 15:19:15 +0200

    ticket v.2

--- js/script.js
+++ js/script.js
@@ -5,15 +5,12 @@ let myBox = document.getElementById('my-box');
 for(x = 1; x < 101; x++) {
     if (x % 3 === 0 && x % 5 === 0){
         console.log('FizzBuzz')
-        x = 'FizzBuzz'
         myBox.append(div)
     } else if(x % 3 === 0){
         console.log('Fizz')
-        x = 'Fizz'
         myBox.append(div)
     } else if(x % 5 === 0){
         console.log('Buzz')
-        x = 'Buzz'
         myBox.append(div)
     } else {
         console.log(x)
@@ -21,15 +18,15 @@ for(x = 1; x < 101; x++) {
 
         div.classList.add('my-small-box')
 
-        if(x === 'FizzBuzz'){
-            div.classList.add('my-bg-orange')
-        } else if(x === 'Fizz'){
-            div.classList.add('my-bg-lightblue')
-        } else if(x === 'Buzz'){
-            div.classList.add('my-bg-green')
-        } else{
-            div.classList.add('my-bg-blueish')
-        }
+        // if(x === 'FizzBuzz'){
+        //     div.classList.add('my-bg-orange')
+        // } else if(x === 'Fizz'){
+        //     div.classList.add('my-bg-lightblue')
+        // } else if(x === 'Buzz'){
+        //     div.classList.add('my-bg-green')
+        // } else{
+        //     div.classList.add('my-bg-blueish')
+        // }
     }
 
 }

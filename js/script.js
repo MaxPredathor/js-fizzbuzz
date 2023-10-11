c638af3ea8ee32f332dec64148d3b651f11187ff
MaxPredathor <max.gilli98@gmail.com>
2023-10-11 14:21:46 +0200

    added js cicle

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let i;
+
+for(i = 1; 1 < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz')
+    } else if(i % 3 === 0){
+        console.log('Fizz')
+    } else if(i % 5 === 0){
+        console.log('Buzz')
+    } else {
+        console.log(i)
+    }
+
+}

2082bef60b9c89df42a6b882a19f3a89cfea8f49
MaxPredathor <max.gilli98@gmail.com>
2023-10-11 16:20:50 +0200

    added ccs styling

--- js/script.js
+++ js/script.js
@@ -9,22 +9,26 @@ for(let x = 1; x < 101; x++) {
         console.log('FizzBuzz') 
         div.classList.add('my-small-box')
         div.classList.add('my-bg-orange')
+        div.innerText =`FizzBuzz`
         myBox.append(div)
     } else if(x % 3 === 0){
         console.log('Fizz')
         div.classList.add('my-small-box')
         div.classList.add('my-bg-lightblue')
+        div.innerText =`Fizz`
         myBox.append(div)
 
     } else if(x % 5 === 0){
         console.log('Buzz')
         div.classList.add('my-small-box')
         div.classList.add('my-bg-green')
+        div.innerText =`Buzz`
         myBox.append(div)
     } else {
         console.log(x)
         div.classList.add('my-small-box')
         div.classList.add('my-bg-blueish')
+        div.innerText =(x)
         myBox.append(div)
     }
 

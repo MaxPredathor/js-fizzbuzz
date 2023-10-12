802397c536dcf7bfb140a1af1fca683a17d42bcf
MaxPredathor <max.gilli98@gmail.com>
2023-10-12 09:49:36 +0200

    js cleaning

--- js/script.js
+++ js/script.js
@@ -1,35 +1,29 @@
-let myBox = document.getElementById('my-box');
+const myBox = document.getElementById('my-box');
 
 for(let x = 1; x < 101; x++) {
 
-    let div = document.createElement("div");
-
-
+    const div = document.createElement("div");
+    div.classList.add('my-small-box')
+    let text, bg;
+    
     if (x % 3 === 0 && x % 5 === 0){
-        console.log('FizzBuzz') 
-        div.classList.add('my-small-box')
-        div.classList.add('my-bg-orange')
-        div.innerText =`FizzBuzz`
-        myBox.append(div)
+        console.log('FizzBuzz') ;
+        bg = 'my-bg-orange';
+        text =`FizzBuzz`;
     } else if(x % 3 === 0){
-        console.log('Fizz')
-        div.classList.add('my-small-box')
-        div.classList.add('my-bg-lightblue')
-        div.innerText =`Fizz`
-        myBox.append(div)
-
+        console.log('Fizz');
+        bg = 'my-bg-lightblue';
+        text =`Fizz`;
     } else if(x % 5 === 0){
-        console.log('Buzz')
-        div.classList.add('my-small-box')
-        div.classList.add('my-bg-green')
-        div.innerText =`Buzz`
-        myBox.append(div)
+        console.log('Buzz');
+        bg = 'my-bg-green';
+        text =`Buzz`;
     } else {
-        console.log(x)
-        div.classList.add('my-small-box')
-        div.classList.add('my-bg-blueish')
-        div.innerText =(x)
-        myBox.append(div)
+        console.log(x);
+        bg = 'my-bg-blueish';
+        text =(x);
     }
-
+    div.classList.add(bg);
+    div.innerText = text;
+    myBox.append(div);
 }
